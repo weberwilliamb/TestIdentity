6d594d7b34320160f58d53393082d282efbcf7b4
William Weber <weber.williamb@gmail.com>
2019-05-01 13:15:51 -0700

    Yay we built the code!

---
+++ TestIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestIdentity.Models;
+
+[assembly: HostingStartup(typeof(TestIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace TestIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TestIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TestIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<TestIdentityContext>();
+            });
+        }
+    }
+}
--- TestIdentity/Startup.cs
+++ TestIdentity/Startup.cs
@@ -51,6 +51,7 @@ namespace TestIdentity
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
